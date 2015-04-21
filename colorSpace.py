db3447e84980d8022b33a558ab14cda2ca0fd04b
washt <washt23@gmail.com>
2015-04-21 15:32:16 -0400

    Got HSV conversion working....kind of.

--- colorSpace.py
+++ colorSpace.py
@@ -11,8 +11,8 @@ cv2.createTrackbar('R','Image',0,255,nothing)
 cv2.createTrackbar('G','Image',0,255,nothing)
 cv2.createTrackbar('B','Image',0,255,nothing)
 # Switch To reorder pixels
-switch = '0: RGB\n1: GBR\n3: HSV'
-cv2.createTrackbar(switch,'Image',0,1,nothing)
+switch = '0: RGB\n1: GBR\n2: HSV'
+cv2.createTrackbar(switch,'Image',0,2,nothing)
 
 print "Starting Color Space Picker...Press ESC to close."
 
@@ -22,5 +22,19 @@ while(1):
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
         break
+    r = cv2.getTrackbarPos('R','Image')
+    g = cv2.getTrackbarPos('G','Image')
+    b = cv2.getTrackbarPos('B','Image')
+
+    s = cv2.getTrackbarPos(switch,'Image')
+
+    HSVimg = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+   
+    if s == 0:
+        image[:] = [r,g,b]
+    elif s == 1:
+        image[:] = [b,g,r,]
+    else:
+        image[:] = HSVimg
 
 cv2.destroyAllWindows()
