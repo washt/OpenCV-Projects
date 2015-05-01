3086f7b129054a74b2a64eac735b0598c6e4030e
washt <washt23@gmail.com>
2015-05-01 12:45:11 -0400

    sift hacking

--- sift.py
+++ sift.py
@@ -1,3 +1,13 @@
 import cv2 as cv
 import numpy as np
 
+img = cv.imread('img/4.2.07.tiff')
+imgGrayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+sift = cv.SIFT()
+
+key_points = sift.detect(gray,None)
+
+img = cv.drawKeypoints(gray,key_points)
+
+cv.imwrite('sift.jpg',img)
