f6041ed4ac1ad2a9f0fb7753ee1cef7992a653cc
washt <washt23@gmail.com>
2015-05-01 11:44:25 -0400

    full bgr range in np arrays

--- colorDetect.py
+++ colorDetect.py
@@ -12,11 +12,15 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of green color in HSV
-    lower_blue = np.array([50,100,100])
-    upper_blue = np.array([70,255,255])
+    lower_blue  = np.array([110,50,50])
+    upper_blue  = np.array([130,255,255])
+    lower_green = np.array([50,100,100])
+    upper_green = np.array([70,255,255])
+    lower_red   = np.array([0,100,100])
+    upper_red   = np.array([20,255,255])
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_red, upper_blue)
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
